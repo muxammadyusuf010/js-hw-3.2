8a83e8faaf6664112820567c418910b95361f3ff
muxammadyusuf010 <119132083+muxammadyusuf010@users.noreply.github.com>
2023-03-11 12:38:42 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+let qty = +prompt('Введите количество')
+let sym = prompt('Введите символ')
+let sym1 = prompt('Введите кончный символ') 
+let sum = ''
+for (let i = 0; i < qty ; i++) {
+	console.log(sum+sym1);
+	sum +=sym
+}
